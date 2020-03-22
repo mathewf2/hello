54de0b5e5a49e0bf5b058ac797063d4dd819588b
Mat <mathewf2@illinois.edu>
2020-03-22 10:57:41 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+    testImplementation("org.slf4j:slf4j-simple:1.7.26")
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
