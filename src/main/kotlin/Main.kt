455da9670a0c975c08320e74c704c9236ce993ac
Mat <mathewf2@illinois.edu>
2020-03-23 11:03:09 -0500

    Add new routes, post requests and tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,14 @@
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -11,22 +17,61 @@ fun hello(): String {
     return "Hello, world!"
 }
 
-fun main() {
-    embeddedServer(Netty, 8008) {
-        routing {
-            get("/") {
-                call.respondText(hello())
-            }
-            get ("/add/{first}/{second}") {
-                try {
-                    val first = call.parameters["first"]!!.toInt()
-                    val second = call.parameters["second"]!!.toInt()
-                    call.respondText((first + second).toString())
-                } catch (e : Exception) {
-                    println(e)
-                    call.respond(HttpStatusCode.BadRequest)
+data class CalculatorRequest(
+    val operation: String, val first: Int, val second: Int
+)
+data class Result(val operation: String, val first: Int, val second: Int, val result: Int)
+
+fun Application.adder() {
+    val counts: MutableMap<String, Int> = mutableMapOf()
+    install(ContentNegotiation) {
+        gson { }
+    }
+    routing {
+        get("/") {
+            call.respondText(hello())
+        }
+        get("/count/{first}") {
+            val firstCount = counts.getOrDefault(call.parameters["first"], 0) + 1
+            counts[call.parameters["first"].toString()] = firstCount
+            call.respondText(firstCount.toString())
+        }
+        post("/calculate") {
+            try {
+                val request = call.receive<CalculatorRequest>()
+                val result = when (request.operation) {
+                    "add" -> request.first + request.second
+                    "subtract" -> request.first - request.second
+                    "multiply" -> request.first * request.second
+                    "divide" -> request.first / request.second
+                    else -> throw Exception("${request.operation} is not supported")
                 }
+                val response = Result(request.operation, request.first, request.second, result)
+                call.respond(response)
+            } catch(e: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
-    }.start(wait = true)
+        get("/{operation}/{first}/{second}") {
+            try {
+                val operation = call.parameters["operation"]!!
+                val first = call.parameters["first"]!!.toInt()
+                val second = call.parameters["second"]!!.toInt()
+                val result = when(operation) {
+                    "add" -> first + second
+                    "subtract" -> first - second
+                    "multiply" -> first * second
+                    "divide" -> first / second
+                    else -> throw Exception("$operation is not supported")
+                }
+                val addResult = Result(operation, first, second, result)
+                call.respond(addResult)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest, e)
+            }
+        }
+    }
+}
+fun main() {
+    embeddedServer(Netty, port = 8008, module = Application::adder).start(wait = true)
 }
